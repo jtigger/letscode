ad53b5b074c50fe094488a1ce4e7d9c30f8e6215
John S. Ryan <jtigger@infosysengr.com>
2013-05-25 06:27:11 -0700

    Tightened down lint tolerances and configured JSHint in IDEA.

--- jakefile.js
+++ jakefile.js
@@ -13,6 +13,21 @@ task("lint", function() {
   allJavaScriptSources.exclude("node_modules");  // assuming these are properly linted, already.
 
   var options = {
+    bitwise: false,
+    curly: true,
+    eqeqeq: true,
+    forin: true,
+    immed: true,
+    latedef: true,
+    newcap: true,
+    noarg: true,
+    nonew: true,
+    undef: true,
+    unused: true,
+    strict: true,
+    trailing: true,
+    maxstatements: 15,
+    maxcomplexity: 10,
     node: true
   };
 
