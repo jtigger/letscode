71cda622f2d08843c879634b470350f42f7bded9
John S. Ryan <jtigger@infosysengr.com>
2013-05-25 05:56:11 -0700

    Automated build now runs lint over all JavaScript in the project.
    + removed the lint runner test because it was failing lint miserably and James said that it wasn't the best code anyhow.

--- jakefile.js
+++ jakefile.js
@@ -1,8 +1,21 @@
+/* global desc, task, jake */
+"use strict";
+
 desc("Full build.");
 task("default", ["lint"]);
 
 desc("Runs JSLint (to catch common JavaScript errors).");
 task("lint", function() {
   var lint_runner = require("./build/lint/lint_runner.js");
-  lint_runner.validateFile("jakefile.js");
+
+  var allJavaScriptSources = new jake.FileList();
+  allJavaScriptSources.include("**/*.js");
+  allJavaScriptSources.exclude("node_modules");  // assuming these are properly linted, already.
+
+  var options = {
+    node: true
+  };
+
+
+  lint_runner.validateFileList(allJavaScriptSources, options);
 });
