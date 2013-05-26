ae813f13c6d8adfa434556a7911d1cff85f74f08
John S. Ryan <jtigger@infosysengr.com>
2013-05-26 16:07:10 -0700

    A simple HTTP server is up and running.

--- specs/server/server.spec.js
+++ specs/server/server.spec.js
@@ -1,11 +1,31 @@
 // Copyright (c) 2013 by John S. Ryan.  All rights reserved.  See LICENSE.txt for details.
 "use strict";
 
+var nodeunit = require("nodeunit");
+var http = require("http");
 var server = require("../../src/server/server.js");
+var httpServer;
+var port = 8000;
 
-exports.firstTest = function(test) {
-  test.expect(1);
+exports.specs = nodeunit.testCase({
+  setUp: function(done) {
+    httpServer = server.start(port);
+    done();
+  },
 
-  test.equals(4, server.number(), "Number is incorrect.");
-  test.done();
-};
+  tearDown: function(done) {
+    httpServer.close();
+    done();
+  },
+
+  "There exists an HTTP server.": function(test) {
+    test.expect(1);
+
+    http.get("http://localhost:" + port, function(response) {
+      test.equals(200, response.statusCode, "Expected server to return the root page; it didn't..");
+      response.on("data", function() {});
+      test.done();
+    });
+  }
+
+});
