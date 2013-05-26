6b2c55ba7b13485ff6d695033954cf8ec02e754d
John S. Ryan <jtigger@infosysengr.com>
2013-05-26 07:19:25 -0700

    First cut at integrating working.
    + currently a semi-automatic process.

--- jakefile.js
+++ jakefile.js
@@ -1,4 +1,4 @@
-/* global desc, task, jake */
+/* global desc, fail, jake, task */
 "use strict";
 
 desc("Full build.");
@@ -12,7 +12,17 @@ task("lint", /* jshint latedef: false */ function() {
   var allJavaScriptSources = new jake.FileList();
   allJavaScriptSources.include("**/*.js");
   allJavaScriptSources.exclude("node_modules");  // assuming these are properly linted, already.
-  lint_runner.validateFileList(allJavaScriptSources, getJSHintOptions());
+  var passesLint = lint_runner.validateFileList(allJavaScriptSources, getJSHintOptions());
+
+  if(!passesLint) { fail("Task 'lint' failed (see above)."); }
+});
+
+desc("Integration");
+task("integrate", ["default"], function() {
+  console.log("Steps for the commit tango...");
+  console.log("1. Verify the build on the master branch, locally.\n   yourbox$ ./jake.sh\n");
+  console.log("2. Pull to the Integation Station and verify the build there.\n   integration$ git pull && ./jake.sh\n");
+  console.log("3. If all good, merge changes into 'last-known-good'\n   yourbox$ merge-to-integration.sh\n");
 });
 
 function getJSHintOptions() {
