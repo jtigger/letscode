5f6540f73b9fa4b6fb77bf3b91442ca1a9471b81
John S. Ryan <jtigger@infosysengr.com>
2013-05-27 17:20:56 -0700

    (refactor) extract HTTP GET calls into utility method in tests.

--- src/specs/server/server.spec.js
+++ src/specs/server/server.spec.js
@@ -8,6 +8,16 @@ var server = require("../../code/server/server.js");
 var httpServer;
 var port = 8000;
 
+function httpGet(url, callback) {
+  http.get(url, function(response) {
+    response.on("data", function() {} );
+    response.on("end", function() {
+      callback(response);
+    });
+  });
+}
+
+
 exports["Given the server is running"] = nodeunit.testCase({
   setUp: function(done) {
     httpServer = server.start(port, done);
@@ -21,7 +31,7 @@ exports["Given the server is running"] = nodeunit.testCase({
     test.expect(1);
 
     http.get("http://localhost:" + port, function(response) {
-      test.equals(200, response.statusCode, "Expected server to return the root page; it didn't..");
+      test.equals(200, response.statusCode, "Expected server to return the root page; it didn't.");
       response.on("data", function() {});
       test.done();
     });
@@ -35,9 +45,12 @@ exports["Given the server is running and has a virtual-to-physical mapping"] = (
 
   return nodeunit.testCase({
     setUp: function(done) {
-      httpServer = server.start(port, done);
-      filename = httpServer.rootDirectory + "/" + pathname + ".html";
-      fs.writeFileSync(filename, "Hello, world.");
+      httpServer = server.start(port, function() {
+        filename = httpServer.rootDirectory + "/" + pathname + ".html";
+        console.log(filename);
+        fs.writeFileSync(filename, "Hello, world.");
+        done();
+      });
     },
 
     tearDown: function(done) {
@@ -48,27 +61,25 @@ exports["Given the server is running and has a virtual-to-physical mapping"] = (
     "when a non-existant file is requested, the server returns a 404.": function(test) {
       test.expect(1);
 
-      http.get("http://localhost:" + port + "/some-non-existant-file", function(response) {
+      httpGet("http://localhost:" + port + "/some-non-existant-file", function(response) {
         test.equals(404, response.statusCode, "Expected an HTTP 404 return code.");
-        response.on("data", function() {} );
-        response.on("end", function() {
-          test.done();
-        });
+        test.done();
       });
     },
 
     "when an existing file is requested, that file is served." : function(test) {
+      test.expect(1);
       var url = "http://localhost:" + port + "/" + pathname;
-      http.get(url, function(response) {
+
+      httpGet(url, function(response) {
         test.equals(200, response.statusCode, "Expected an HTTP 200 (OK) response; virtual path = " + url + "; file located at " + filename);
-        response.on("data", function() {} );
-        response.on("end", function() {
-          test.done();
-        });
+        test.done();
       });
+
     }
   });
 })();
+
 
 exports["In general"] = nodeunit.testCase({
   "when started without a port number, throws an exception": function(test) {
