dc81057ba17cdf671277abf2382890c9ee8f7b96
John S. Ryan <jtigger@infosysengr.com>
2013-06-03 06:57:05 -0700

    Build file now runs package installer to ensure all dependencies are installed.

--- jake.sh
+++ jake.sh
@@ -1,4 +1,13 @@
+#/bin/bash
+
 clear
+
+# designate a listening port for our app
 export PORT=5000
 echo $PORT
+
+# Make sure all dependencies are downloaded/installed
+npm install
+
+# build
 node_modules/.bin/jake $*
