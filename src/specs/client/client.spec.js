aec69ed8f7be9bc1131cc365e0bcc4199c9a86de
John S. Ryan <jtigger@infosysengr.com>
2013-06-04 17:02:16 -0700

    Now running client-side tests
    + Split unit tests into two suites: client-side and server-side.
    + introducing karma + mocha + expect.js for client-side testing.

---
+++ src/specs/client/client.spec.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2013 by John S. Ryan.  All rights reserved.  See LICENSE.txt for details.
+"use strict";
+
+describe("dummy", function() {
+  it("should execute", function() {
+    expect("foo").to.eql("foo");
+  });
+});
