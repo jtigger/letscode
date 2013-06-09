381f1ecebcafa106653d6581111fe6c8970d7cd5
John S. Ryan <jtigger@infosysengr.com>
2013-06-09 05:41:52 -0700

    Now Karma runs in-process so as to avoid OS differences in process handling.
    + the procstreams solution wasn't working on Windoze.  The karma runner process would quit before all the output could be captured.
    + includes a temporary monkey patch on the write function of the standard output stream because Karma doesn't return the output of the tests, just an exit code.
    + pushed all output into the jake logger
    + all tasks now display a banner for easier reading of build output.

--- jakefile.js
+++ jakefile.js
@@ -4,7 +4,8 @@
 var BUILD_DIR = "build";
 var TEST_TEMP_DIR = BUILD_DIR + "/test";
 var lint_runner = require("./src/build/lint/lint_runner.js");
-var $p = require("procstreams");
+var path = require("path");
+
 var supportedBrowsers = [
   "IE 7.0 (Windows)",
   "IE 8.0 (Windows)",
@@ -45,33 +46,73 @@ function getJSHintOptionsForBrowsers() {
   return  options;
 }
 
-var karmaCommand = "node node_modules/.bin/karma ";
+jake.logger.logTaskStart = function(taskName) {
+  var border = new Array(taskName.length+1).join("=");
+  this.log("\n" + border);
+  this.log(taskName);
+  this.log(border);
+};
 
-function karma(command, done) {
-  var testRunnerCommand = karmaCommand + command;
-  $p(testRunnerCommand, {out: true}).data(function(err, stdout) {
-    if (err) {
-      done(err);
-    } else {
-      done(stdout.toString());
+function patchInStdoutSniffer() {
+  var originalWriteFunction = process.stdout.write;
+  var sniffer = {
+    output : "",
+    unpatch : function() {
+      process.stdout.write = originalWriteFunction;
     }
-  });
+  };
+
+  process.stdout.write = function(data) {
+    sniffer.output += data;
+    originalWriteFunction.apply(this, arguments);
+  };
+
+  return sniffer;
 }
+
+var karma = (function() {
+  var homedir = path.resolve(process.cwd(), "node_modules", "karma", "lib");
+
+  return {
+    start : function (configFilepath, done) {
+      var config = {configFile : configFilepath };
+      require(path.join(homedir, "server")).start(config);
+      done();
+    },
+
+    run : function (done) {
+      var sniffer = patchInStdoutSniffer();
+      var config = {};
+      require(path.join(homedir, "runner")).run(config, function(exitCode) {
+        // karma runner is not designed to be run programmatically and sends it output directly to standard out.
+        sniffer.unpatch();
+        if(exitCode) {
+          done(new Error("Karma runner reported an error #" + exitCode + ".  Output = \"" + sniffer.output + "\"."));
+        } else {
+          done(sniffer.output);
+        }
+      });
+    }
+  };
+})();
 
 directory(BUILD_DIR);
 directory(TEST_TEMP_DIR);
 
 desc("Integration");
 task("integrate", ["default"], function() {
-  console.log("Steps for the commit tango...");
-  console.log("1. Verify the build on the master branch, locally.\n   yourbox$ ./jake.sh\n");
-  console.log("2. Pull to the Integation Station and verify the build there.\n   integration$ git pull && ./jake.sh\n");
-  console.log("3. If all good, merge changes into 'last-known-good'\n   yourbox$ merge-to-integration.sh\n");
+  jake.logger.logTaskStart("integrate");
+  jake.logger.log("Steps for the commit tango...");
+  jake.logger.log("1. Verify the build on the master branch, locally.\n   yourbox$ ./jake.sh\n");
+  jake.logger.log("2. Pull to the Integation Station and verify the build there.\n   integration$ git pull && ./jake.sh\n");
+  jake.logger.log("3. If all good, merge changes into 'last-known-good'\n   yourbox$ merge-to-integration.sh\n");
 });
 
 desc("Starts the Karma server (which captures browsers for client-side testing).");
-task("karma", [], function() {
-  karma("start src/build/karma.conf.js", function(results) {
+task("setup.client-tests", [], function() {
+  jake.logger.logTaskStart("setup.client-tests");
+
+  karma.start("src/build/karma.conf.js", function(results) {
     if(results instanceof Error) {
       fail("Failed to start Karma server with \"" + results.toString() + "\" (also, see above).");
     }
@@ -83,6 +124,7 @@ task("karma", [], function() {
 desc("Runs server-side unit tests.");
 task("test.unit.server", [TEST_TEMP_DIR], function() {
   // CAP-0002
+  jake.logger.logTaskStart("test.unit.server");
   var reporter = require('nodeunit').reporters.default;
   var allJavaScriptTests = new jake.FileList();
   allJavaScriptTests.include("src/specs/server/*.js");
@@ -98,7 +140,8 @@ task("test.unit.server", [TEST_TEMP_DIR], function() {
 
 desc("Runs client-side unit tests.");
 task("test.unit.client", [], function() {
-  karma("run", function(results) {
+  jake.logger.logTaskStart("test.unit.client");
+  karma.run(function(results) {
     if(results instanceof Error) {
       fail("Client-side tests failed with \"" + results.toString() + "\" (also, see above).");
     }
@@ -117,6 +160,8 @@ task("test.unit", ["test.unit.server", "test.unit.client"]);
 desc("Runs production verification tests.");
 task("test.prod", [TEST_TEMP_DIR], function() {
   // CAP-0002
+  jake.logger.logTaskStart("test.prod");
+
   var reporter = require('nodeunit').reporters.default;
   var productionTests = new jake.FileList();
   productionTests.include("src/tests/production_smoke_test.js");
@@ -131,7 +176,7 @@ desc("Runs JSLint on everything");
 task("lint", ["lint.server", "lint.client"]);
 
 task("lint.server", function() {
-  console.log("\nLinting server-side code...");
+  jake.logger.logTaskStart("lint.server");
 
   var serverSideJavaScriptFiles = new jake.FileList();
   serverSideJavaScriptFiles.include("src/**/server/**/*.js");
@@ -145,7 +190,7 @@ task("lint.server", function() {
 });
 
 task("lint.client", function() {
-  console.log("\nLinting client-side code...");
+  jake.logger.logTaskStart("lint.client");
 
   var clientSideJavaScriptFiles = new jake.FileList();
   clientSideJavaScriptFiles.include("src/**/client/**/*.js");
@@ -157,11 +202,14 @@ task("lint.client", function() {
 
 desc("Purges the build output directory (i.e. " + BUILD_DIR + ").");
 task("clean", function() {
+  jake.logger.logTaskStart("clean");
   jake.rmRf(BUILD_DIR);
 });
 
-task("check node version", function() {
+task("check.dependencies", function() {
   // TODO: obtain node version from the package.json definition.
+  jake.logger.logTaskStart("check.dependencies");
+
   var expectedNodeVersion = "v0.10.8";
   var actualNodeVersion = process.version;
 
@@ -172,5 +220,5 @@ task("check node version", function() {
 });
 
 desc("Full build.");
-task("default", ["check node version", "clean", "lint", "test.unit"]);
+task("default", ["check.dependencies", "clean", "lint", "test.unit"]);
 
