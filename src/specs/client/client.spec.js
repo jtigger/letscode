fccbf07483c045c415c2a911b3b3eb1b4a24f652
John S. Ryan <jtigger@infosysengr.com>
2013-06-12 07:40:05 -0700

    (refactor) make "drawing area" an explicit fixture of this test suite.

--- src/specs/client/client.spec.js
+++ src/specs/client/client.spec.js
@@ -1,27 +1,27 @@
 // Copyright (c) 2013 by John S. Ryan.  All rights reserved.  See LICENSE.txt for details.
-/* globals afterEach, beforeEach, describe, expect, it, wwp, $  */
+/* globals afterEach, beforeEach, describe, expect, it, wwp, $ */
 
 (function() {
   "use strict";
 
   describe("wee canvas", function() {
+    var drawingArea;
 
     beforeEach(function() {
-      var drawingAreaDiv = document.createElement("div");
-      drawingAreaDiv.setAttribute("id", "wwp-drawingArea");
-      document.body.appendChild(drawingAreaDiv);
+      drawingArea = $("<div id='wwp-drawingArea'></div>");
+      $(document.body).append(drawingArea);
     });
 
     afterEach(function() {
-      $("#wwp-drawingArea").remove();
+      drawingArea.remove();
     });
 
     it("should exist within the drawing div", function() {
       var drawingAreaChildTag;
 
-      wwp.initializeDrawingArea("wwp-drawingArea");
+      wwp.initializeDrawingArea(drawingArea[0]);
 
-      drawingAreaChildTag = $("#wwp-drawingArea").children()[0].tagName.toLowerCase();
+      drawingAreaChildTag = drawingArea.children()[0].tagName.toLowerCase();
       // if a browser does not support SVG (e.g. IE 8), the canvas will be a DIV tag.
       // otherwise, it will be a SVG tag.
       if(drawingAreaChildTag !== "svg" && drawingAreaChildTag !== "div") {
@@ -32,9 +32,9 @@
     it("should match the dimensions of the enclosing div", function() {
       var paper;
 
-      $("#wwp-drawingArea").height(200).width(400);
+      drawingArea.height(200).width(400);
 
-      paper = wwp.initializeDrawingArea("wwp-drawingArea");
+      paper = wwp.initializeDrawingArea(drawingArea[0]);
 
       expect(paper.height).to.be(200);
       expect(paper.width).to.be(400);
