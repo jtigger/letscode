968b4b0e2f1814d819e9285c08ba5e98e297a8b4
John S. Ryan <jtigger@infosysengr.com>
2013-06-16 08:02:34 -0700

    CLI startup now includes lib dir consistently.

--- weewikipaint.sh
+++ weewikipaint.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-WEB=src/spikes/003-how-to-manipulate-paths-with-raphael/web
-#WEB=src/code/web
+WEB_ROOT=src/spikes/003-how-to-manipulate-paths-with-raphael/web
+#WEB_ROOT=src/code/web
 
-ln -s /Users/john/development/projects/jtigger/javascript/letscode/lib src/code/web/scripts/lib
-node src/code/server/weewikipaint.js 5000 $WEB
-rm src/code/web/scripts/lib
+ln -s /Users/john/development/projects/jtigger/javascript/letscode/lib ${WEB_ROOT}/scripts/lib
+node src/code/server/weewikipaint.js 5000 ${WEB_ROOT}
+rm ${WEB_ROOT}/scripts/lib
